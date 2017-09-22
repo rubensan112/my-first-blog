ae06c3ad0b6a5af92454f66b4affcaecf656e904
ruben <rubensan112@hotmail.com>
2017-09-22 03:40:39 +0200

    Add registration system, publisch, edit, views posts

--- angel/urls.py
+++ angel/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^$', views.home_pagev3, name='home_pagev3'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
     url(r'', views.not_found, name='not_found'),
 
 ]
--- angel/views.py
+++ angel/views.py
@@ -9,10 +9,60 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required #Para el tema de requerir login
 
 # Create your views here.
-def home_page(request):
-    return render(request, 'angel/base_test.html', {})
+def home_pagev3(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'angel/home_pagev3.html',{'posts': posts })
+
+
 def not_found(request):
     return render(request, 'angel/not_found.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'angel/post_detail.html', {'post': post})
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('home_pagev3')
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'angel/post_edit.html', {'form': form})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST) #request. Post son los datos que envia el usuario del formulario. Construimos el modelo del formulario Postform con esos datos.
+        if form.is_valid():
+            post = form.save(commit=False) #El commit=false quiere decir, que aun no queremos guardar el modelo Post
+            post.author = request.user #Forzamos el author
+            post.save()
+            return redirect('post_detail', pk=post.pk) #Este nuevo post que se ha creado, tendra un pk, lo cogemos para que nos diriga a el.
+    else:
+        form = PostForm()
+    return render(request, 'angel/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'angel/post_draft_list.html', {'posts': posts})
 
 
 
